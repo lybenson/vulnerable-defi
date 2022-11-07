fa4c0dd771967f5316a3ff8bc8ddae9e9345e342
lybenson <naivebenson@gmail.com>
2022-11-07 18:39:33 +0800

    update

--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -4,8 +4,8 @@ const { expect } = require('chai')
 describe('[Challenge] Truster', function () {
   let deployer, attacker
 
+  // eth 转换为 wei 乘以 1000000 * 10^18
   const TOKENS_IN_POOL = ethers.utils.parseEther('1000000')
-
   before(async function () {
     /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
     [deployer, attacker] = await ethers.getSigners()
@@ -16,6 +16,7 @@ describe('[Challenge] Truster', function () {
     this.token = await DamnValuableToken.deploy()
     this.pool = await TrusterLenderPool.deploy(this.token.address)
 
+    // 向 pool 合约地址转入 1000000 eth
     await this.token.transfer(this.pool.address, TOKENS_IN_POOL)
 
     expect(
